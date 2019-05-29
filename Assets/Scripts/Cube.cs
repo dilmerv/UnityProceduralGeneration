63efabfa8500055b1c949ed775ea03f41df4c50f
Dilmer Valecillos <dilmervst@gmail.com>
2019-05-29 07:23:12 -0600

    Added new features for default and random materials, also stats

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -150,43 +150,10 @@ public class Cube : Shape
         normals[22] = right;
         normals[23] = right;
 
-        // Step 4 UVs
-        /* 
-        Vector2[] uvs = new Vector2[vertices.Length];
-        for (int i = 0; i < uvs.Length; i++)
-        {
-            uvs[i] = new Vector2(Mathf.Abs(vertices[i].x), Mathf.Abs(vertices[i].z));
-        }
-        */
-
-        // TODO For a future video
-        Vector2[] uvs = new Vector2[vertices.Length];
-
-        // Iterate over each face (here assuming triangles)
-        for(int index = 0; index < triangles.Length; index += 3)
-        {
-            // Get the three vertices bounding this triangle.
-            Vector3 v1 = vertices[triangles[index]];
-            Vector3 v2 = vertices[triangles[index + 1]];
-            Vector3 v3 = vertices[triangles[index + 2]];
-
-            // Compute a vector perpendicular to the face.
-            Vector2 normal = Vector3.Cross(v3 - v1, v2 - v1);
-
-            // Form a rotation that points the z+ axis in this perpendicular direction.
-            // Multiplying by the inverse will flatten the triangle into an xy plane.
-            Quaternion rotation = Quaternion.Inverse(Quaternion.LookRotation(normal));
-
-            // Assign the uvs, applying a scale factor to control the texture tiling.
-            uvs[triangles[index]]     = (Vector2)(rotation * v1) * 0.5f;
-            uvs[triangles[index + 1]] = (Vector2)(rotation * v2) * 0.5f;
-            uvs[triangles[index + 2]] = (Vector2)(rotation * v3) * 0.5f;
-        }
-        
         mesh.vertices = vertices;
         mesh.triangles = triangles;
         mesh.normals = normals;
-        //mesh.uv = uvs;
+        
         mesh.Optimize();
 
         return mesh;
--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
 
 public class Grid : MonoBehaviour
 {
@@ -11,6 +14,8 @@ public class Grid : MonoBehaviour
     [SerializeField,Range(1,200)]
     private int height = 1;
     private int prevHeight = 1;
+
+    private float constructionDelay = 1.0f;
 
     private GameObject[,] grid = null;
 
@@ -37,8 +42,20 @@ public class Grid : MonoBehaviour
     private string shaderName = "Lightweight Render Pipeline/Lit";
 
     [SerializeField]
-    private Texture buildingTexture;
+    private bool applyProceduralMaterial = false;
 
+    [SerializeField]
+    private Material defaultMaterial = null;
+
+    #region UI Bindings
+
+    [SerializeField]
+    private Text numOfShapesText = null;
+
+    [SerializeField]
+    private Text lengthOfTimeText = null;
+
+    #endregion
     void Start()
     {    
         prevWidth = width;
@@ -52,43 +69,67 @@ public class Grid : MonoBehaviour
 
     void BuildGrid()
     {
+        numOfShapesText.text = $"{width * height}";
+
+        DateTime started = DateTime.Now;
+        
         for(int row = 0; row < height; row++)
         {
             for(int col = 0; col < width; col++)
             {
-                GameObject cell = null;
-                if(grid[row, col] == null)
-                {
-                    cell = new GameObject($"cell_{row}_{col}");
-                    cell.transform.parent = gameObject.transform;
-                    grid[row, col] = cell;
-                }   
-                else {
-                    DestroyImmediate(grid[row, col]);
-                    grid[row, col] = new GameObject($"cell_{row}_{col}");
-                    cell = grid[row, col];
-                    cell.transform.parent = gameObject.transform;
-                }
-
-                cell.transform.position = 
-                    new Vector3(shapeWidth * row,
-                                shapeHeight * Random.Range(1.0f, maxRandomHeight),
-                                shapeDepth * col);
-
-                MeshFilter meshFilter = cell.AddComponent<MeshFilter>();
-                MeshRenderer renderer = cell.AddComponent<MeshRenderer>();
-                meshFilter.mesh = new Cube {
-                    Width = shapeWidth,
-                    Height = shapeHeight,
-                    Depth = shapeDepth
-                }.Generate();
-                
-                renderer.ApplyRandomMaterial(shaderName, cell.name);
-
-                renderer.material.EnableKeyword ("_BaseMap");
-                renderer.material.SetTexture("_BaseMap", buildingTexture);
+                StartCoroutine(AddCell(row, col));
             }
         }
+
+        DateTime ended = DateTime.Now;
+        TimeSpan diff = ended - started;
+        if(diff.Seconds == 0)
+            lengthOfTimeText.text = $"{diff.Milliseconds} milliseconds";
+        else if(diff.Seconds < 100)
+            lengthOfTimeText.text = $"{diff.Seconds}.{diff.Milliseconds} seconds";
+        else 
+            lengthOfTimeText.text = $"{diff.Minutes}.{diff.Seconds} minutes";
+            
+    }
+    
+    IEnumerator AddCell(int row, int col)
+    {
+        GameObject cell = null;
+        if(grid[row, col] == null)
+        {
+            cell = new GameObject($"cell_{row}_{col}");
+            cell.transform.parent = gameObject.transform;
+            grid[row, col] = cell;
+        }   
+        else {
+            DestroyImmediate(grid[row, col]);
+            grid[row, col] = new GameObject($"cell_{row}_{col}");
+            cell = grid[row, col];
+            cell.transform.parent = gameObject.transform;
+        }
+
+        cell.transform.position = 
+            new Vector3(shapeWidth * row,
+                        shapeHeight * Random.Range(1.0f, maxRandomHeight),
+                        shapeDepth * col);
+
+        MeshFilter meshFilter = cell.AddComponent<MeshFilter>();
+        MeshRenderer renderer = cell.AddComponent<MeshRenderer>();
+
+        Cube cube = new Cube {
+            Width = shapeWidth,
+            Height = shapeHeight,
+            Depth = shapeDepth
+        };
+        
+        meshFilter.mesh = cube.Generate();
+        
+        if(applyProceduralMaterial)
+            renderer.ApplyRandomMaterial(shaderName, cell.name);
+        else
+            renderer.material = defaultMaterial;
+
+        yield return null;
     }
 
     void Update()
@@ -106,6 +147,8 @@ public class Grid : MonoBehaviour
             prevWidth = width;
             prevHeight = height;
             grid = new GameObject[height, width];
+            
+            BuildGrid();
         }
     }
 
