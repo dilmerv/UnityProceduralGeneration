73091e393a8aaf703f6492d001950f68f4756602
Dilmer Valecillos <dilmervst@gmail.com>
2019-06-03 13:53:15 -0700

    Added scriptable object for procedural params

--- Assets/Scripts/ProceduralOptions.cs
+++ Assets/Scripts/ProceduralOptions.cs
@@ -3,5 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ProceduralParams", menuName = "Procedural/Parameters", order = 0)]
 public class ProceduralOptions : ScriptableObject 
 {
+    [SerializeField,Range(1,200)]
+    private int width = 1;
     
 }
